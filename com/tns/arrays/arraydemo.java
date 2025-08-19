1e98668e5e5086f09ecd8fbd900ae9dba18ebc22
Yuvaraj Madugu <yuvaraj6207@gmail.com>
2025-08-19 21:09:58 +0530

    Add files via upload

---
+++ com/tns/arrays/arraydemo.java
@@ -0,0 +1,12 @@
+package com.tns.arrays;
+
+public class arraydemo {
+    public static void main(String[] args) {
+        //initialization:
+        int[] arr = new int[3];
+        arr[0] = 10;
+        arr[1] = 5;
+        arr[2] = 9;
+        System.out.println(arr[1]);
+    }
+}
--- com/tns/oops/abstraction/interfaces/UpiPaymentsMain.java
+++ com/tns/oops/abstraction/interfaces/UpiPaymentsMain.java
@@ -3,9 +3,9 @@ package com.tns.oops.abstraction.interfaces;
 public class UpiPaymentsMain {
     public static void main(String[] args) {
         Payment ob = new UpiPayments();
-        Payment.paymentMethod(); //static method
 
-        ob.payments();
+        Payment.paymentMethod(); //static method
+        ob.payments(); //abstract method
         ob.paymentReceipt(150.0);
 
     }
