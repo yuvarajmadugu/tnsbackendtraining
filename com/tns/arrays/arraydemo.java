557de56a2032d720ea915939357e8cade90b01ae
Yuvaraj Madugu <yuvaraj6207@gmail.com>
2025-08-20 22:21:10 +0530

    Update arraydemo.java

--- com/tns/arrays/arraydemo.java
+++ com/tns/arrays/arraydemo.java
@@ -8,5 +8,12 @@ public class arraydemo {
         arr[1] = 5;
         arr[2] = 9;
         System.out.println(arr[1]);
+
+
+        //accessing of elements using for each loop:
+        for(int num: arr){
+            System.out.println(num};
+        }
     }
 }
+
