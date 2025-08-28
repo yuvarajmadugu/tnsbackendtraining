feb7678c3d097a8f8312429f3eef645ac23df700
Yuvaraj Madugu <yuvaraj6207@gmail.com>
2025-08-28 23:20:00 +0530

    Add files via upload

---
+++ com/tns/UsageThread.java
@@ -0,0 +1,10 @@
+package com.tns.threads;
+
+public class UsageThread extends Thread{
+    public synchronized void mul(int n){
+        for(int i=0; i<11; i++){
+            int result = n*i;
+            System.out.println(result);
+        }
+    }
+}
