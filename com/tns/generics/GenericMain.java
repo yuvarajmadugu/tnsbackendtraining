5d86527cfcd9089e6c5881f742b3be8cf38ce31a
Yuvaraj Madugu <yuvaraj6207@gmail.com>
2025-09-17 21:36:04 +0530

    Add files via upload

--- com/tns/generics/GenericMain.java
+++ com/tns/generics/GenericMain.java
@@ -1,1 +1,13 @@
+package com.tns.generics;
 
+public class GenericMain {
+    public static void main(String[] args) {
+        GenericDemo<String> ob1 = new GenericDemo<>();
+        ob1.setMsg("Hii");
+        System.out.println(ob1.getMsg());
+
+        GenericDemo<Integer> ob2 = new GenericDemo<>();
+        ob2.setMsg(1);
+        System.out.println(ob2.getMsg());
+    }
+}
