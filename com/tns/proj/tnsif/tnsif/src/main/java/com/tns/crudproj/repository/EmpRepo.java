66121e4fac1c09f6b0d8be08ab4c1056938971e4
Yuvaraj Madugu <yuvaraj6207@gmail.com>
2025-09-20 22:37:54 +0530

    Delete com/tns/proj/tnsif/tnsif/src/main/java/com/tns/crudproj/repository/EmpRepo.java

--- com/tns/proj/tnsif/tnsif/src/main/java/com/tns/crudproj/repository/EmpRepo.java
+++
@@ -1,8 +0,0 @@
-package com.tns.crudproj.repository;
-
-import com.tns.crudproj.entity.Emp;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmpRepo extends JpaRepository<Emp, Integer> {
-    //to be continued
-}
